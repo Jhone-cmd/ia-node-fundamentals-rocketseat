2a7afd32ff9c5256414e21ff39fb2c41a262bbe4
jhone-cmd <jhone.1941@gmail.com>
2025-11-24 10:11:07 -0300

    integrating with OpenAI, generating a response.

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,20 @@
+import OpenAI from 'openai'
 import { env } from './env/schema.ts'
 
-console.log('API Key', env.API_KEY)
+const client = new OpenAI({
+  apiKey: env.API_KEY,
+})
+
+await client.chat.completions
+  .create({
+    model: 'gpt-4o-mini',
+    messages: [
+      {
+        role: 'user',
+        content: 'Escreva uma mensagem de uma frase sobre nodejs',
+      },
+    ],
+  })
+  .then((completion) => {
+    console.log('completion:', completion.choices[0].message.content)
+  })
