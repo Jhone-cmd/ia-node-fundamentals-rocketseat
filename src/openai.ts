aec7bed09a21913fa317d57c02e64a8815b1a5e7
jhone-cmd <jhone.1941@gmail.com>
2025-11-26 10:37:44 -0300

    optimizing function calling with recursion

--- src/openai.ts
+++ src/openai.ts
@@ -45,10 +45,10 @@ const tools: ChatCompletionTool[] = [
   },
 ];
 
-const generateCompletion = async (
+async function generateCompletion(
   messages: ChatCompletionMessageParam[],
   format: any
-) => {
+) {
   const completion = await client.chat.completions.parse({
     model: 'gpt-4o-mini',
     max_tokens: 100,
@@ -61,15 +61,39 @@ const generateCompletion = async (
     throw new Error('Refusal');
   }
 
+  const { tool_calls } = completion.choices[0].message;
+  if (tool_calls) {
+    const [tool_call] = tool_calls;
+    const toolsMap = {
+      produtos_em_estoque: produtosEmEstoque,
+      produtos_em_falta: produtosEmFalta,
+    };
+    const functionToCall = toolsMap[tool_call.function.name];
+    if (!functionToCall) {
+      throw new Error('Function not found');
+    }
+    const result = functionToCall(tool_call.function.arguments);
+    messages.push(completion.choices[0].message);
+    messages.push({
+      role: 'tool',
+      tool_call_id: tool_call.id,
+      content: result.toString(),
+    });
+    const completionWithToolResult: any = await generateCompletion(
+      messages,
+      zodResponseFormat(schemaProducts, 'produtos_schema')
+    );
+    return completionWithToolResult;
+  }
   return completion;
-};
+}
 
-export const generateProducts = async (message: string) => {
+export async function generateProducts(message: string) {
   const messages: ChatCompletionMessageParam[] = [
     {
       role: 'developer',
       content: `
-          - Liste 3 produtos que atendam a necessidade do usuário.
+          - Liste no máximo 3 produtos que atendam a necessidade do usuário.
           - Considere apenas os produtos disponíveis em estoque.
           - Responda em JSON no formato { products: string[] }`,
     },
@@ -84,30 +108,5 @@ export const generateProducts = async (message: string) => {
     zodResponseFormat(schemaProducts, 'produtos_schema')
   );
 
-  const { tool_calls } = completion.choices[0].message;
-  if (tool_calls) {
-    const [tool_call] = tool_calls;
-    const toolsMap = {
-      produtos_em_estoque: produtosEmEstoque,
-      produtos_em_falta: produtosEmFalta,
-    };
-    const functionToCall = toolsMap[tool_call.function.name];
-    if (!functionToCall) {
-      throw new Error('Function not found');
-    }
-    const result = functionToCall(tool_call.function.parsed_arguments);
-    messages.push(completion.choices[0].message);
-    messages.push({
-      role: 'tool',
-      tool_call_id: tool_call.id,
-      content: result.toString(),
-    });
-    const newCompletion = await generateCompletion(
-      messages,
-      zodResponseFormat(schemaProducts, 'produtos_schema')
-    );
-    return newCompletion.choices[0].message.parsed;
-  }
-
   return completion.choices[0].message.parsed;
-};
+}
